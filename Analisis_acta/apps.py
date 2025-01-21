44d6d1fa4734ace34eb64731591ebe17de8697c1
joluro86 <joluro86@hotmail.com>
2025-01-21 07:41:02 -0500

    recuperacion carpeta

---
+++ Analisis_acta/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AnalisisActaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'analisis_acta'
---
+++ Analisis_acta/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from analisis_acta.views import *
+urlpatterns = [
+    path('subir_acta_revision/', subir_acta_revision, name="subir_acta_revision"),
+    path('limpiar_novedades/', limpiar_novedades, name="limpiarnov"),
+    path('limpiar_acta/', limpiar_acta, name="limpiaracta"),
+    path('calculo_novedades/', calculo_novedades_acta, name="calculo_novedades"),
+    path('novedades-acta/', novedades_acta, name="novedades_acta"),
+    path('novedades-acta/', novedades_acta, name="novedades_acta"),        
+] +static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+
+
